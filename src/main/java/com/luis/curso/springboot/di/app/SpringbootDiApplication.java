0c5456e2cb772b375c21771adf371b6357f3ab3e
advanceuser <luisalbertorg117@gmail.com>
2024-07-02 16:33:01 -0600

    init commit

---
+++ src/main/java/com/luis/curso/springboot/di/app/SpringbootDiApplication.java
@@ -0,0 +1,13 @@
+package com.luis.curso.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiApplication.class, args);
+	}
+
+}
